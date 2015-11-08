b1e67f4879242639a8cf7e06c6054df9be722059
Amaury <amauryleclerc@hotmail.fr>
2015-11-08 14:05:08 +0100

    conf spring

--- src/main/java/fr/jjouenne/repository/CardRepository.java
+++ src/main/java/fr/jjouenne/repository/CardRepository.java
@@ -3,7 +3,6 @@ package fr.jjouenne.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.jjouenne.entity.Card;
-
 public interface CardRepository extends JpaRepository<Card, Long> {
 	
 	public Card findOneByLibelle(String libelle);
--- src/test/java/fr/jjouenne/test/CardTest.java
+++ src/test/java/fr/jjouenne/test/CardTest.java
@@ -1,6 +1,7 @@
 package fr.jjouenne.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
