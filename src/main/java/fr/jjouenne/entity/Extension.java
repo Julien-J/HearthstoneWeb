c56391abf880a1230f2196c7c1ad911d3737cab7
Julien <Julien@192.168.1.38>
2016-03-06 18:44:03 +0100

    Ajout entité extension

---
+++ src/main/java/fr/jjouenne/entity/Extension.java
@@ -0,0 +1,39 @@
+package fr.jjouenne.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="extension")
+public class Extension
+{
+	@Id
+	private Long id;
+	
+	@Column(name="wording")
+	private String wording;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getWording() {
+		return wording;
+	}
+	public void setWording(String wording) {
+		this.wording = wording;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
